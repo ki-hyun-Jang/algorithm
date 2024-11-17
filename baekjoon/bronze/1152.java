a20b39bc859ab3c448d9e59aa9598abefbe38ce7
ki-hyun-Jang <skjwj0511@gmail.com>
2024-11-17 23:12:29 +0900

    1152수정

--- baekjoon/bronze/1152.java
+++ baekjoon/bronze/1152.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        String inputs = " "+br.readLine();
+        String inputs = br.readLine();
         String[] input = inputs.split(" ");
         int cnt = 0;
 
