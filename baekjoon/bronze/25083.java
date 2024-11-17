578c927d141ca394ece64ff6f5402bf5d090bb0c
ki-hyun-Jang <skjwj0511@gmail.com>
2024-11-17 20:41:32 +0900

    25083

---
+++ baekjoon/bronze/25083.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String str = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        System.out.print(str);
+    }
+}
